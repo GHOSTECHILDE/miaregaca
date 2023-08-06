b425909e5ec869d12c96be26bf8d5a4956122268
GHOSTECHILDE <141500147+GHOSTECHILDE@users.noreply.github.com>
2023-08-06 11:14:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    window.location.href = "confirm.html";
+  });
+  
+  document.getElementById("noButton").addEventListener("click", function() {
+    window.location.href = "reject.html";
+  });
+
